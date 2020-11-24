71d711889faf302f6da53e747b3173fcc2da13ed
Yuki-Kanamori <y.kanamory@gmail.com>
2020-11-24 11:43:37 +0900

    revised the area data and dens data

--- make_data.R
+++ make_data.R
@@ -3,9 +3,11 @@ require(maps)
 require(mapdata)
 require(ggplot2)
 
+dir1 = "/Users/Yuki/Dropbox/sokouo1/make_VASTdata" 
 # catch ---------------------------------------------------------
-setwd("/Users/Yuki/Dropbox/sokouo1/make_VASTdata/catch_raw")
-path = "/Users/Yuki/Dropbox/sokouo1/make_VASTdata/catch_raw"
+dir = "/Users/Yuki/Dropbox/sokouo1/make_VASTdata/catch_raw"
+setwd(dir)
+path = dir
 files = list.files(path)
 
 catch = NULL
@@ -17,7 +19,10 @@ summary(catch) #NA無し
 catch = catch[, c(-1, -6)]
 colnames(catch)
 colnames(catch) = c("station", "depth", "ami", "in_out", "species", "raw_catch_kg", "raw_N", "file", "year")
+
+setwd(dir1)
 write.csv(catch, "raw_catch.csv", fileEncoding = "CP932")
+
 
 
 # area ----------------------------------------------------------
@@ -37,23 +42,32 @@ area = area[, c(-1:-3, -15:-166)]
 colnames(area)
 colnames(area) = c("sokutei", "gyokaku_koritu", "nanboku", "syukei_tanni", "syukei_tanni_mesyo", "area_A", "station", "depth", "ami", "swept_area", "raw_N", "file", "year")
 summary(area) #NA無し
+
+setwd(dir1)
 write.csv(area, "area.csv", fileEncoding = "CP932")
 
 
 # bind ----------------------------------------------------------
 catch2 = catch %>% select(-file) %>% mutate(tag = paste(year, station, depth, ami, sep = "_"))
+catch3 = catch2 %>% group_by(tag) %>% summarize(catch_kg = sum(raw_catch_kg))
+catch3 = catch3 %>% mutate(year = as.numeric(str_sub(tag, 1, 4)), station = str_sub(tag, 6, 6), depth = as.numeric(str_sub(tag, 8,10)), ami = as.numeric(str_sub(tag, 12, 12)))
+
 area2 = area %>% select(-file) %>% mutate(tag = paste(year, station, depth, ami, sep = "_")) %>% select(-year, -station, -depth, -ami)
 
-length(unique(catch2$tag))
+#catchとareaの位置情報の数が合わない
+length(unique(catch3$tag))
 length(unique(area2$tag))
 
-tag = merge
+c_tag = data_frame(tag = unique(catch3$tag), data_c = 1)
+a_tag = data_frame(tag = unique(area2$tag), data_a = 1)
+check = merge(c_tag, a_tag, by = "tag", all = T)
+# =>2019年のデータを確認する必要がある！
 
-catch2 = merge(catch2, area2, by = "tag", all = T)
-summary(catch2)
-catch2$raw_N.x-catch2$raw_N.y #catchに入っている尾数とareaに入っている尾数のデータが合わない！！！
+catch3 = merge(catch3, area2, by = "tag", all = T)
+summary(catch3)
+catch2$raw_N-catch2$raw_N #catchに入っている尾数とareaに入っている尾数のデータが合わない！！！
 
-setwd("/Users/Yuki/Dropbox/sokouo1/make_VASTdata/catch_raw")
+setwd(dir1)
 write.csv(catch2, "catch_area.csv", fileEncoding = "CP932")
 
 
@@ -73,18 +87,19 @@ rec = rec[, c(2:4,9:12,22:23)]
 colnames(rec)
 colnames(rec) = c("station", "depth", "ami", "la1", "la2", "lo1", "lo2", "file", "year")
 rec = rec %>% mutate(lon = lo1+lo2/60, lat = la1+la2/60) %>% select(-la1, -la2, -lo1, -lo2, file)
+
+setwd(dir1)
 write.csv(rec, "lonlat_rec.csv", fileEncoding = "CP932")
 
 rec2 = rec %>% mutate(tag = paste(year, station, depth, sep = "_")) %>% select(-depth, -ami, -file)
 summary(rec2)
 
 # mean density --------------------------------------------------
-dens = catch2
-dens$dens = dens$raw_catch_kg/dens$swept_area
+dens = catch3
+dens$dens = dens$catch_kg/dens$swept_area
 summary(dens)
-dens = dens %>% group_by(tag) %>% summarise(dens = sum(dens))
-dens = dens %>% mutate(year = as.numeric(str_sub(tag, 1, 4)), station = str_sub(tag, 6, 6), depth = as.numeric(str_sub(tag, 8,10)), ami = as.numeric(str_sub(tag, 12, 12)))
 
+# 網次の扱いが分からない=>平均をとっておく
 dens2 = dens %>% group_by(year, station, depth) %>% summarise(mean_dens = mean(dens)) %>% mutate(tag = paste(year, station, depth, sep = "_")) 
 dens2$year = NULL
 dens2$station = NULL
@@ -94,8 +109,44 @@ summary(dens2)
 write.csv(dens2, "dens_lonlat.csv", fileEncoding = "CP932")
 
 
+# add the area in each station and depth ------------------------
+dens2 = dens2 %>% select(-tag) %>% mutate(tag = paste(station, depth, sep = "_"))
+area3 = area %>% mutate(tag = paste(station, depth, sep = "_")) %>% select(station, depth, tag, area_A)
+area_A = area3 %>% distinct(tag, .keep_all = T)
+
+setwd(dir1)
+write.csv(area_A, "area_A.csv", fileEncoding = "CP932")
+
+# Aが変だから，2019年のデータを使う
+setwd("/Users/Yuki/Dropbox/sokouo1/make_VASTdata/area")
+a2019 = read.csv("area2019.csv")
+a2019 = a2019[, c(-1:-3, -15:-166)]
+colnames(a2019)
+a2019 = a2019 %>% select(面積.Km2., STATIONコード, 水深) %>% dplyr::rename(area_A = 面積.Km2., station = STATIONコード, depth = 水深) %>% na.omit() %>% mutate(tag = paste(station, depth, sep = "_")) %>% distinct(tag, .keep_all = T)
+setwd(dir1)
+write.csv(a2019, "area_A2019.csv", fileEncoding = "CP932")
+
+st_dep = data_frame(tag = paste(dens2$station, dens2$depth, sep = "_")) %>% distinct(tag, .keep_all = T)
+length(unique(st_dep$tag))
+
+
+# bind(A~Hのみ)
+setwd(dir1)
+r_area = read.csv("area_revised.csv") %>% mutate(tag = paste(station, depth, sep = "_"))
+head(dens2)
+dens3 = dens2 %>% select(-tag) %>% mutate(tag = paste(station, depth, sep = "_"))
+head(dens3)
+head(r_area)
+
+data = inner_join(dens3, r_area, by = "tag")
+unique(data$station.x)
+unique(data$depth.x)
+summary(data)
+
+max = data[, data$mean_dens == 3284.968]
 
 # map -----------------------------------------------------------
+# 調査を行なった場所
 p <- ggplot() + coord_fixed() +
   xlab("Longitude") + ylab("Latitude")
 world_map <- map_data("world")
@@ -104,11 +155,11 @@ jap_cog <- jap[jap$lat > 36 & jap$lat < 41.5 & jap$long > 140.6 & jap$long < 143
 t2 <- p + geom_polygon(data = jap_cog, aes(x=long, y=lat, group=group), colour="gray 50", fill="gray 50")
 # + coord_map(xlim = c(139.5, 140.3), ylim = c(35, 35.75))
 t2 + geom_point(data = rec2, aes(x = lon, y = lat), shape = 16, size = 1)+facet_wrap(~year, ncol = 12) + theme_bw()
+t2 + geom_point(data = data, aes(x = lon, y = lat), shape = 16, size = 1)+facet_wrap(~year, ncol = 12) + theme_bw()
 # t2 + geom_point(data = rec2, aes(x = lon, y = lat, colour = station), shape = 16, size = 1)+facet_wrap(~year, ncol = 8) + theme_bw()
 
-
-dens3 = dens2 %>% na.omit()
-t2 + geom_point(data = dens3, aes(x = lon, y = lat, colour = mean_dens), shape = 16, size = 1)+facet_wrap(~year, ncol = 12) + scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))+ theme_bw()
+# 密度
+t2 + geom_point(data = data %>% na.omit(), aes(x = lon, y = lat, colour = mean_dens), shape = 16, size = 1)+facet_wrap(~year, ncol = 12) + scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))+ theme_bw()
 
 # catch only
 t2 + geom_point(data = dens3 %>% filter(mean_dens > 0), aes(x = lon, y = lat, colour = mean_dens), shape = 16, size = 1)+facet_wrap(~year, ncol = 12) + scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))+ theme_bw()
