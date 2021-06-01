da2bd4904f066923abbbfb35cc0280fde6e8679f
Yuki-Kanamori <y.kanamory@gmail.com>
2021-06-01 11:24:05 +0900

    changed?

--- NatL.R
+++ NatL.R
@@ -5,7 +5,8 @@ path = "/Users/Yuki/Dropbox/sokouo1/make_VASTdata/Numbers"
 files = list.files(path)
 kiti = NA
 for (i in 1:length(files)){
-  data = read.csv(paste0(files[i])) %>% filter(和名 == "キチジ") %>% mutate(file = paste0(files[i])) %>% mutate(year = as.numeric(str_sub(file, 5, 8)))
+  i = 1
+  data = read.csv(paste0(files[i]), fileEncoding = "SJIS") %>% filter(和名 == "キチジ") %>% mutate(file = paste0(files[i])) %>% mutate(year = as.numeric(str_sub(file, 5, 8)))
   # unique(data$和名)
   kiti = rbind(kiti, data)
 }
